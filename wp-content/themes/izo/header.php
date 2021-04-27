b9cdd35fd04365d86a054cad2e069f06f904073a
David Amorim Xavier (VSCODE) <webxddesign@hotmail.com>
2021-04-27 02:10:17 -0300

    Mascara CPF

--- wp-content/themes/izo/header.php
+++ wp-content/themes/izo/header.php
@@ -16,6 +16,9 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<script src="jquery-1.5.2.min.js"></script>
+	<script src="jquery.maskedinput-1.3.min.js"></script>
+
 
 	<?php wp_head(); ?>
 </head>
